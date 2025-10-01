5b0ea2a75db60b909ced466f5ab6e1bb869624df
-_- <111021816+atsugula@users.noreply.github.com>
2025-10-01 14:24:49 -0500

    add model libros

--- app/models/Libro.php
+++ app/models/Libro.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Libro extends Model
 {
     use HasFactory;
+
+    protected $table = 'libros';
+
+    protected $fillable = [
+        'codigo',
+        'titulo',
+        'autor',
+        'anio_publicacion',
+        'categoria',
+        'estado',
+    ];
+
+    /**
+     * Relación con los préstamos
+     * Un libro puede estar en muchos préstamos.
+     */
+    public function prestamos()
+    {
+        return $this->hasMany(Prestamo::class, 'libro_id');
+    }
 }
